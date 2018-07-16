7b4d511c29319d6bb9ff7d7688e1e8e6713716b2
edward <bauchiroad@gmail.com>
2018-07-16 12:41:26 +0100

    added devise user

---
+++ test/models/user_test.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+   setup do
+    @first_user = users(:one)
+   end
+
+   test "user should be valid with name, email and password" do
+     assert @first_user.valid?
+   end
+
+   test "user should be invalid without email" do
+     @first_user.email = ''
+     refute @first_user.valid?
+     assert_not_nil @first_user.errors[:email]
+   end
+
+   test "user should be invalid without name" do
+     @first_user.name = ''
+     refute @first_user.valid?
+     assert_not_nil @first_user.errors[:name]
+   end
+
+
+end
