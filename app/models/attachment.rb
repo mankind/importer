bab71a62275c1d31933965fe2795a44fdafe452b
edward <bauchiroad@gmail.com>
2018-07-24 15:04:29 +0100

    cleaned code from attachment model

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -7,25 +7,8 @@ class Attachment < ApplicationRecord
   after_initialize :generate_token
 
   mount_uploader :csv_file, CsvUploader
-=begin
+
   def self.import(file)
-    csv_name = file.original_filename
-    Book.transaction do
-      attachment = Current.user.attachments.build(csv_file: file, original_csv_filename: csv_name)
-
-      CSV.foreach(file.path, headers: true) do |row|
-        merged_params = row.to_hash.merge(csv_name: csv_name)
-        Current.user.books.create! merged_params
-      end
-
-      attachment.save
-      url = attachment.csv_file.url
-      CsvNotificationJob.perform_later(url)
-    end
-  end
-=end
-
-   def self.import(file)
     Attachment.transaction do
       @attachment = create_attachment_fom_csv(file)
       create_books_from_csv(file)
